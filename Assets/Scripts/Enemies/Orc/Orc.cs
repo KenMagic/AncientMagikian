0b6c1b541ec81b6d942cf85472da0c4f6d3718d2
LamNV290304 <nguyenvietlam290304@gmail.com>
2025-07-02 15:39:03 +0700

    Fix Orc

--- Assets/Scripts/Enemies/Orc/Orc.cs
+++ Assets/Scripts/Enemies/Orc/Orc.cs
@@ -1,30 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Orc : MonoBehaviour
 {
-    [SerializeField]
-    public EnemySO enemyData; // Reference to the EnemySO scriptable object containing enemy data
+    [SerializeField] public EnemySO enemyData;
 
     public Animator animator;
-
     public StateMachine stateMachine;
+
     public Transform target;
     public Transform towerTarget;
 
-    private bool isChasing = false; // Flag to check if the orc is chasing the target
-    public float chaseSpeed;
-    public float distanceToTarget = 0.5f; // Distance to the target to trigger attack
+    public float distanceToTarget = 0.5f;
     public LayerMask targetLayerMask;
+    public float chaseSpeed;
+
+    public bool isChasing = false;
+
+    private Coroutine stopChaseCoroutine;
 
     private void Awake()
     {
-        chaseSpeed = enemyData.speed * 2; // Set chase speed from the enemy data
+        chaseSpeed = enemyData.speed * 2;
         stateMachine = GetComponent<StateMachine>();
         animator = GetComponent<Animator>();
 
-        GameObject tower = GameObject.FindGameObjectWithTag("Tower");
-        if (tower == null)
-            target = tower.transform;
+        towerTarget = GameObject.FindGameObjectWithTag("Tower").transform;
+        target = towerTarget;
     }
 
     private void Start()
@@ -34,16 +36,16 @@ public class Orc : MonoBehaviour
             Debug.LogError("Orc missing essential components");
             return;
         }
-        towerTarget = GameObject.FindGameObjectWithTag("Tower").transform;
-        stateMachine.SetState(new OrcMoveState(animator, this));
-        
-    }
 
+        stateMachine.SetState(new OrcMoveState(animator, this));
+    }
 
     private void Update()
     {
+        Debug.DrawRay(transform.position, (target.position - transform.position).normalized * distanceToTarget, Color.red);
         stateMachine.Update();
-        if (isChasing)
+
+        if (isChasing && target != null)
         {
             float distance = Vector2.Distance(transform.position, target.position);
             if (distance <= distanceToTarget)
@@ -53,48 +55,50 @@ public class Orc : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    public void StartChasing(Transform player)
     {
-        if (other.CompareTag("Player"))
+        if (stopChaseCoroutine != null)
         {
-            Debug.Log("Player vào vùng phát hiện");
-            target = other.transform;
-            isChasing = true;
-            stateMachine.SetState(new OrcMoveState(animator, this)); // Đuổi theo player
-            return;
-        }
-        //Detect player
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Entered Player");
-            stateMachine.SetState(new OrcAttackState(animator, this, enemyData.attackCooldown));
-            return;
+            StopCoroutine(stopChaseCoroutine);
+            stopChaseCoroutine = null;
         }
 
-        //Detect tower
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, other.transform.position - transform.position, distanceToTarget, targetLayerMask);
-        if (hit.collider != null && hit.collider.CompareTag("Tower"))
-        {
-            Debug.Log("Entered Tower");
-            stateMachine.SetState(new OrcAttackState(animator, this, enemyData.attackCooldown));
-        }
+        Debug.Log("Orc: Bắt đầu đuổi Player");
+        target = player;
+        isChasing = true;
+        stateMachine.SetState(new OrcMoveState(animator, this));
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    public void StopChasingAfterDelay(float delay = 0.5f)
     {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player rời khỏi vùng");
-            target = towerTarget; // Quay lại đuổi tower
-            isChasing = false;
-            stateMachine.SetState(new OrcMoveState(animator, this));
-            return;
-        }
+        if (stopChaseCoroutine == null)
+            stopChaseCoroutine = StartCoroutine(StopChase(delay));
+    }
 
-        if (other.CompareTag("Tower"))
+    private IEnumerator StopChase(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Orc: Ngừng đuổi, quay về tower");
+        target = towerTarget;
+        isChasing = false;
+        stateMachine.SetState(new OrcMoveState(animator, this));
+        stopChaseCoroutine = null;
+    }
+
+    public void TryAttackTower(Transform tower)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            tower.position - transform.position,
+            distanceToTarget,
+            targetLayerMask
+        );
+
+        if (hit.collider != null && hit.collider.CompareTag("Tower"))
         {
-            Debug.Log("Exited Tower");
-            stateMachine.SetState(new OrcMoveState(animator, this));
+            Debug.Log("Orc: Attack Tower");
+            target = tower;
+            stateMachine.SetState(new OrcAttackState(animator, this, enemyData.attackCooldown));
         }
     }
 
--- Assets/Scripts/Enemies/Orc/OrcPOV.cs
+++ Assets/Scripts/Enemies/Orc/OrcPOV.cs
@@ -1,22 +1,38 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class OrcPOV : MonoBehaviour
 {
-    //public Transform Target;
-    //public void OnTriggerEnter2D(Collider2D collider)
-    //{
-    //    if (collider.gameObject.CompareTag("Player"))
-    //    {
-    //        Orc enemyMovement = GetComponentInParent<Orc>();
-    //        enemyMovement.SetTarget(collider.transform);
-    //    }
-    //}
-    //public void OnTriggerExit2D(Collider2D collider)
-    //{
-    //    if (collider.gameObject.CompareTag("Player"))
-    //    {
-    //        Orc enemyMovement = GetComponentInParent<Orc>();
-    //        enemyMovement.SetTarget(Target);
-    //    }
-    //}
+    private Orc orc;
+
+    private void Awake()
+    {
+        orc = GetComponentInParent<Orc>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (orc == null) return;
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("OrcPOV: Phát hiện Player");
+            orc.StartChasing(other.transform);
+        }
+        else if (other.CompareTag("Tower"))
+        {
+            Debug.Log("OrcPOV: Phát hiện Tower");
+            orc.TryAttackTower(other.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (orc == null) return;
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("OrcPOV: Player rời vùng");
+            orc.StopChasingAfterDelay(); // sẽ delay 2s trong Orc.cs
+        }
+    }
 }
